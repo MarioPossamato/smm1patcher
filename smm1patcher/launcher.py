b19c3083c66c3ee7776cad5efd9b84478084bbe2
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2020-08-18 18:28:26 -0400

    Delete launcher.py

--- smm1patcher/launcher.py
+++
@@ -1,9 +0,0 @@
-import os, tkinter
-from tkinter import filedialog
-
-home = os.path.expanduser('~')
-root = tkinter.Tk()
-root.withdraw()
-print('Select A Super Mario Maker 1 Binary File.')
-binary_path = filedialog.askopenfilename(initialdir = home + "/Desktop",title = "Select A Super Mario Maker 1 Binary File",filetypes = ([("All Files","*.*")]))
-os.system('py -i smm1patcher.py ' + binary_path)
